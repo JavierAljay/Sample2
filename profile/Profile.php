3e8632de8bc5da779c76eb9127c682f03e19567c
Aljay Javier <125188239+JavierAljay@users.noreply.github.com>
2023-09-23 10:05:18 +0800

    Add files via upload

---
+++ profile/Profile.php
@@ -0,0 +1,78 @@
+<?php
+?>
+<html>
+<title>Javier Profile</title>
+<head>
+<link rel="stylesheet" href="css\Layout.css">
+<link rel="stylesheet" href="css\Gallery.css">
+</head>
+<body bgcolor="#808080">
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">Hobbies</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+<div class="row">
+	<div class="column">
+    <h1>
+        Aljay B Javier
+	    </h1>
+	<p>20 year's old living in Quezon City</p>	
+		<p >Currently 3rd year college in <br>
+	Quezon City University taking a program of BSIT<br>    
+</p>
+	</div>
+		<div class="column">
+		<div class="gallery">
+
+    <img src="img\1.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\1.jpg">
+    <img src="img\1.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\2.jpg">
+    <img src="img\2.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\3.jpg">
+    <img src="img\3.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\4.jpg">
+    <img src="img\4.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\5.jpg">
+    <img src="img\5.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="img\6.jpg">
+    <img src="img\6.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+
+
+    <p >Currently 3rd year college in <br>
+	Quezon City University taking a program of BSIT<br>    
+</p>
+</div>
+</div>
+</body>
+</html>
